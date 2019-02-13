a658a898b9e52ed44a02c815e47dd3eb80e574ac
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-02-12 23:59:35 -0300

    Updates

---
+++ precache-manifest.9e5b0c8724173a7a00f97a3d453eefcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef52b207b229b1abcff1",
+    "url": "%20//PabloPaglilla.github.io/md-previewer/static/js/runtime~main.ef52b207.js"
+  },
+  {
+    "revision": "463d9c0d9524c560de2b",
+    "url": "%20//PabloPaglilla.github.io/md-previewer/static/js/main.463d9c0d.chunk.js"
+  },
+  {
+    "revision": "5317222eff7e8c5b673b",
+    "url": "%20//PabloPaglilla.github.io/md-previewer/static/js/2.5317222e.chunk.js"
+  },
+  {
+    "revision": "463d9c0d9524c560de2b",
+    "url": "%20//PabloPaglilla.github.io/md-previewer/static/css/main.f1228a25.chunk.css"
+  },
+  {
+    "revision": "8b85544e5980879b9782cdfcd3277aa5",
+    "url": "%20//PabloPaglilla.github.io/md-previewer/index.html"
+  }
+];
